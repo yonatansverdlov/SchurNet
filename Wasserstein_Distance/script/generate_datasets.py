11d8a347c81f6b31fb1c4d5f9c1671d5d6c29e85
STAFF\yonatans <yonatansverdlov276@gmail.com>
2024-10-03 21:32:11 +0300

    Initial commit

---
+++ Wasserstein_Distance/script/generate_datasets.py
@@ -0,0 +1,120 @@
+import argparse
+import json
+import itertools
+import numpy as np
+import random
+from dataset import *
+import os
+
+np.random.seed(0)
+
+def create_dataset(dataset_name:str,is_small:bool):
+    if dataset_name == 'ncircle3':
+        train_sz = 2000
+        val_sz = 200
+        nmax = 300 if is_small else 500 
+        nmin = 100 if is_small else 300
+        dim = 3
+    elif dataset_name =='ncircle6':
+        train_sz = 3600
+        val_sz = 400
+        nmax = 300 if is_small else 500 
+        nmin = 100 if is_small else 300
+        dim = 6
+    elif dataset_name == 'random':
+        nmax = 257 if is_small else 300
+        nmin = 256 if is_small else 200
+        train_sz = 3000
+        val_sz = 300
+        dim = 2
+    elif dataset_name == 'mn_small':
+        nmin = 20 if is_small else 300
+        nmax = 200 if is_small else 500
+        train_sz = 3000
+        val_sz = 300
+        dim = 3
+    elif dataset_name == 'mn_large':
+        train_sz = 4000
+        val_sz = 400
+        nmax = 2049 if is_small else 2000
+        nmin = 2048 if is_small else 1800
+        dim = 3
+    elif dataset_name == 'rna':
+        nmin = 20 if is_small else 300
+        nmax = 200 if is_small else 500
+        train_sz = 3000
+        val_sz = 300
+        dim = 2000
+
+    if dataset_name in ['random', 'ncircle3', 'ncircle6']:
+
+        if 'ncircle' in dataset_name:
+            Ps, Qs, dists = noisy_circles(nmin=nmin, 
+                                        nmax=nmax, 
+                                        pairs=train_sz,
+                                        dim=dim,
+                                        order=2)
+            Ps_val, Qs_val, dists_val = noisy_circles(nmin=nmin, 
+                                                    nmax=nmax, 
+                                                    pairs=val_sz,
+                                                    dim=dim,
+                                                    order = 2)                                       
+        else:
+            n = 10000
+            pointset = fixed_point_set(dim=dim, num=n, data_type=dataset_name)
+            Ps, Qs, dists = build_dataset(pointset, 
+                                        nmin=nmin, 
+                                        nmax=nmax, 
+                                        pairs=train_sz)
+            Ps_val, Qs_val, dists_val = build_dataset(pointset, 
+                                                    nmin=nmin, 
+                                                    nmax=nmax, 
+                                                    pairs=val_sz)
+                                            
+    elif dataset_name in ['mn_small','mn_large']:
+        # Loading raw ModelNet data
+        raw_data, labels, label_dict = load_hdf5_data('../data/samples/raw/modelnet.h5')
+        # build train dataset
+        Ps, Qs, dists = build_comprehensive_sampler(raw_data, 
+                                                    label_dict, 
+                                                    nmin=nmin, 
+                                                    nmax=nmax, 
+                                                    pairs=train_sz + val_sz,
+                                                    order=2)
+        print(len(Ps), len(Qs))
+        Ps_val, Qs_val, dists_val = Ps[train_sz: train_sz + val_sz], Qs[train_sz: train_sz + val_sz], dists[train_sz: train_sz + val_sz]
+
+
+    elif dataset_name =='rna':
+        pointset = np.load('../data/samples/raw/rna.npy')
+        Ps, Qs, dists = build_dataset(pointset, 
+                                        nmin=nmin, 
+                                        nmax=nmax, 
+                                        pairs=train_sz,
+                                        order=2)
+        Ps_val, Qs_val, dists_val = build_dataset(pointset, 
+                                                    nmin=nmin, 
+                                                    nmax=nmax, 
+                                                    pairs=val_sz,
+                                                    order = 2)
+
+
+    top_lvl = '../data/samples/{}'.format(dataset_name)
+    if not os.path.exists(top_lvl):
+        os.makedirs(top_lvl)
+    train_sf = '../data/samples/{}/train_{}.npz'.format(dataset_name,'small' if is_small else 'lrage' )
+    val_sf = '../data/samples/{}/val_{}.npz'.format(dataset_name,'small' if is_small else 'lrage' )
+    np.savez(train_sf, Ps=Ps[:train_sz], Qs=Qs[:train_sz], dists=dists[:train_sz])
+    np.savez(val_sf, Ps=Ps_val, Qs=Qs_val, dists=dists_val)
+
+parser = argparse.ArgumentParser(description='construct datasets')
+parser.add_argument('--dataset_name', type=str)
+
+arg = parser.parse_args()
+
+create_dataset(dataset_name=arg.dataset_name,is_small=True)
+
+create_dataset(dataset_name=arg.dataset_name,is_small=False)
+
+
+
---
+++ Wasserstein_Distance/script/train_wd.py
@@ -0,0 +1,120 @@
+# todo
+# train and get similar results as in the paper
+# add our shared layers
+# train again and check results
+import os
+import numpy as np
+import torch
+import argparse
+import yaml
+import torch.nn as nn
+from models import SharedProductNet
+from fit_productnet import train_point_productnet, validation_loss
+import itertools
+from easydict import EasyDict
+from utils import SetTypes, EMDPairDataset
+
+def return_dataset(set_type,small ):
+    train_sf, val_sf = set_type.return_path(small=small)                                                            
+    train_data = np.load(train_sf, allow_pickle=True)
+    Ps = train_data['Ps']
+    Qs = train_data['Qs']
+    dists = train_data['dists']
+    val_data = np.load(val_sf, allow_pickle=True)
+    Ps_val = val_data['Ps']
+    Qs_val = val_data['Qs']
+    dists_val = val_data['dists']
+    # Create dataset
+    train_dataset = EMDPairDataset(Ps, Qs, torch.tensor(dists))
+    val_dataset = EMDPairDataset(Ps_val, Qs_val, dists_val)
+    return train_dataset, val_dataset
+
+def train_wd(set_type):
+    if set_type == 'ncircle3':
+        set_type = SetTypes.ncircle3
+    elif set_type == 'ncircle6':
+        set_type = SetTypes.ncircle6
+    elif set_type == 'random':
+        set_type = SetTypes.random
+    elif set_type == 'mn_small':
+        set_type = SetTypes.mn_small
+    elif set_type == 'mn_large':
+        set_type = SetTypes.mn_large
+    elif set_type == 'rna':
+        set_type = SetTypes.rna
+    dimension = set_type.return_dim()
+    dataset_name = set_type.return_dataset_name()
+
+    with open('config.yaml', 'r') as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)[dataset_name]
+        config = EasyDict(config)
+    seed = config.seed
+    torch.manual_seed(seed)
+
+    # If using CUDA, set the seed for all GPUs
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed) 
+
+    train_dataset, val_dataset = return_dataset(set_type=set_type,small = True)
+    # now train
+    embed_size = config.embed_size
+    num_layer = config.num_layer
+    mlp_params = {'hidden': embed_size, 'output': embed_size, 'layers':num_layer}
+    phi_params = {'hidden': embed_size, 'output': embed_size, 'layers': num_layer}
+    rho_params = {'hidden': embed_size, 'output': 1, 'layers':num_layer}
+    embedding_size = phi_params['output']
+    modelname = 'try_shared'
+    max_iter = 40
+    shared = True
+    device = 'cuda'
+    factor = config.factor
+    lr = config.lr
+    wd = config.wd
+    size = config.batch_size
+    use_bn = config.use_bn
+    opt_type = config.opt_type
+    act = config.act
+    slope = config.slope
+
+    shared_model, epochs_trained = train_point_productnet(train_dataset= train_dataset,
+                                                val_dataset= val_dataset,
+                                                dimension= dimension,
+                                                initial= mlp_params,
+                                                phi = phi_params,
+                                                rho = rho_params,
+                                                device=device,
+                                                lr=lr,
+                                                name=modelname,
+                                                iterations=max_iter,
+                                                batch_size=size,                              
+                                                activation=act, 
+                                                batch=use_bn,
+                                                wd = wd,
+                                                factor=factor,
+                                                opt_type=opt_type,
+                                                slope=slope)
+    val_loss_mean, val_loss_std = validation_loss(val_dataset, shared_model, device, image=False)
+    path_to_model =f'../data/models/{dataset_name}'
+    if not os.path.exists(path_to_model):
+        os.makedirs(path_to_model)
+    path_to_model = os.path.join(path_to_model,f'Model_{val_loss_mean}')
+    torch.save(shared_model.state_dict(),path_to_model)
+    val_loss,_ = validation_loss(model=shared_model, val_dataset=val_dataset, device='cuda')
+    print(f"The test loss on the small disterbution is {val_loss.item()}")
+    # Check generalization.
+    train_dataset, val_dataset = return_dataset(set_type=set_type,small = False)
+    val_loss_gen,_ = validation_loss(model=shared_model, val_dataset=val_dataset, device='cuda')
+    print(f"The test loss on the out of disterbution is {val_loss_gen.item()}")
+    print(path_to_model)
+
+
+parser = argparse.ArgumentParser(description="Process dataset with a specified radius.")
+
+# Add dataset_name as a string argument (positional)
+parser.add_argument('--dataset_type', type=str, help='Name of the dataset',default='ncircle3')
+
+# Parse the arguments
+args = parser.parse_args()
+
+train_wd(set_type=args.dataset_type)
